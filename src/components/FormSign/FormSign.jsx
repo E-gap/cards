26551540c5cced83b50acfe9e10a5d94016b1674
E-gap <106596181+E-gap@users.noreply.github.com>
2023-06-19 23:05:16 +0300

    confirm password show password

--- src/components/FormSign/FormSign.jsx
+++ src/components/FormSign/FormSign.jsx
@@ -1,24 +1,49 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-
+import { useState } from 'react';
 import css from './FormSign.module.css';
+
+import { BiShow } from 'react-icons/bi';
 
 const SignupSchema = Yup.object().shape({
   email: Yup.string().required('Please input email'),
   password: Yup.string().required(),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref('password'), null],
+    'Passwords must match'
+  ),
 });
 
 function FormSign() {
+  const [typePassword, setTypePassword] = useState('password');
+  const [typeConfirmPassword, setTypeConfirmPassword] = useState('password');
+
   const submitForm = (values, actions) => {
     console.log(values);
     console.log(actions);
     actions.resetForm();
   };
 
+  const toggleShowPassword = () => {
+    if (typePassword === 'password') {
+      setTypePassword('text');
+    } else {
+      setTypePassword('password');
+    }
+  };
+
+  const toggleShowConfirmPassword = () => {
+    if (typeConfirmPassword === 'password') {
+      setTypeConfirmPassword('text');
+    } else {
+      setTypeConfirmPassword('password');
+    }
+  };
+
   return (
     <Formik
-      initialValues={{ email: '', password: '' }}
+      initialValues={{ email: '', password: '', confirmPassword: '' }}
       validationSchema={SignupSchema}
       onSubmit={submitForm}
     >
@@ -30,15 +55,22 @@ function FormSign() {
         </label>
         <label className={css.label}>
           Password
-          <Field type="password" name="password" />
+          <Field type={typePassword} name="password" />
+          <BiShow className={css.buttonHide} onClick={toggleShowPassword} />
           <ErrorMessage name="password" component="div" />
         </label>
         <label className={css.label}>
           Confirm password
-          <Field type="password" name="confirmpassword" />
-          <ErrorMessage name="confirmpassword" component="div" />
+          <Field type={typeConfirmPassword} name="confirmPassword" />
+          <BiShow
+            className={css.buttonHide}
+            onClick={toggleShowConfirmPassword}
+          />
+          <ErrorMessage name="confirmPassword" component="div" />
         </label>
-        <button type="submit">Submit</button>
+        <button type="submit" className={css.submit}>
+          Submit
+        </button>
       </Form>
     </Formik>
   );
