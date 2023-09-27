255a39a76143a5ff8467455c47287489b25af79b
Evhen Gaponyuk <e-gap@ukr.net>
2023-09-27 22:29:19 +0300

    form sign complete

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
@@ -10,11 +11,13 @@ export const register = createAsyncThunk(
   async (userData, thunkApi) => {
     try {
       const { data } = await instance.post('users/register', userData);
-      console.log(data);
+      Notiflix.Notify.success('You are registered');
       instance.defaults.headers.common.Authorization = `Bearer ${data.token}`;
-      console.log(data);
       return data;
     } catch (error) {
+      Notiflix.Notify.failure(
+        'Registration failed, please check the credentials'
+      );
       return thunkApi.rejectWithValue(error.message);
     }
   }
@@ -25,9 +28,11 @@ export const login = createAsyncThunk(
   async (userData, thunkApi) => {
     try {
       const { data } = await instance.post('users/login', userData);
+      Notiflix.Notify.success('You are loged in');
       instance.defaults.headers.common.Authorization = `Bearer ${data.token}`;
       return data;
     } catch (error) {
+      Notiflix.Notify.failure('Login failed, please check the credentials');
       return thunkApi.rejectWithValue(error.message);
     }
   }
@@ -36,6 +41,7 @@ export const login = createAsyncThunk(
 export const logout = createAsyncThunk('auth/logout', async (_, thunkApi) => {
   try {
     await instance.post('/users/logout');
+    Notiflix.Notify.success('You are loged out');
     instance.defaults.headers.common.Authorization = ``;
   } catch (error) {
     return thunkApi.rejectWithValue(error.message);
